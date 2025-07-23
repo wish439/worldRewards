b7d395d9b035bf7ab3f20d026427c9037a588680
wishtoday238 <wishtoday238@gmail.com>
2025-07-23 20:16:29 +0800

    Initial commit

--- src/main/java/org/wishtoday/egar/worldRewards/WorldRewards.java
+++ src/main/java/org/wishtoday/egar/worldRewards/WorldRewards.java
@@ -1,17 +1,32 @@
 package org.wishtoday.egar.worldRewards;
 
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.wishtoday.egar.worldRewards.Command.WRSCommand;
+import org.wishtoday.egar.worldRewards.Events.ChangeWorldEvent;
 
 public final class WorldRewards extends JavaPlugin {
-
+    private static WorldRewards plugin;
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        plugin = this;
+        this.getServer().getPluginManager().registerEvents(new ChangeWorldEvent(), this);
+        saveDefaultConfig();
+        registerCommands();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        super.onDisable();
+    }
+    public static WorldRewards getInstance() {
+        return plugin;
+    }
+    private void registerCommands() {
+        this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, event -> {
+            Commands registrar = event.registrar();
+            WRSCommand.registerCommand(registrar);
+        });
     }
 }
